d40685f91affb16e0d4d93969c8c9592dfea98fc
Anastasios Selalmazidis <t.selalmasidis@gmail.com>
2014-11-04 00:01:21 +0200

    Add template.php which includes header.php and footer.php

---
+++ template.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: tasos
+ * Date: 11/3/14
+ * Time: 11:56 PM
+ */
+
+include_once('backend/header.php'); ?>
+
+    <!-- Here goes the content of the body -->
+
+<? include_once('backend/footer.php') ?>

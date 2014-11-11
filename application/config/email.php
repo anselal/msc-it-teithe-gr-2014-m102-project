da727f174e41c0845aeda7b594aed9621bcebeb4
Anastasios Selalmazidis <t.selalmasidis@gmail.com>
2014-11-12 00:20:33 +0200

    Add email config and Controller

---
+++ application/config/email.php
@@ -0,0 +1,15 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/**
+ * Created by IntelliJ IDEA.
+ * User: tasos
+ * Date: 11/11/14
+ * Time: 11:39 PM
+ */
+
+$config['protocol'] = 'smtp';
+$config['smtp_host'] = 'ssl://smtp.gmail.com';
+$config['smtp_port'] = 465;
+$config['smtp_user'] = 't.selalmasidis@gmail.com';
+$config['smtp_pass'] = 'Oth3rsid324u14ME!';
+$config['smtp_timeout'] = '4';
+$config['charset'] = 'iso-8859-7';
---
+++ application/controllers/email.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: tasos
+ * Date: 11/11/14
+ * Time: 7:09 PM
+ */
+
+class Email extends CI_Controller {
+
+    function __construct() {
+        parent::__construct();
+    }
+
+    function index() {
+        $this->load->library('email');
+
+        $this->email->set_newline("\r\n");
+
+        $this->email->from('t.selalmasidis@gmail.com', 'Anastasios Selalmazidis');
+        $this->email->to('t.selalmasidis@gmail.com');
+        $this->email->subject('This is an email test');
+        $this->email->message('It is working. Great!');
+
+        $file = "/home/tasos/public_html/m102/license.txt" ;
+
+        $this->email->attach($file);
+
+        if($this->email->send()) { // send() will return 1 if succesfull
+            echo 'Your email was sent, fool.';
+        }
+        else {
+            show_error($this->email->print_debugger());
+        }
+
+
+    }
+} 
